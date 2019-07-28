3be9ccd851833f273fe180359b52aa528f63338a
Liam <lbonnage@gmail.com>
2019-07-28 13:43:40 -0400

    Adding detailed dropdown descriptions

--- static/scripts/dropdownScript.js
+++ static/scripts/dropdownScript.js
@@ -2,7 +2,7 @@ $(function () {
     $("#serverDropdown").on("change", function () {
         switch ($(this).val()) {
             case "1":
-                $("#serverText").html("html/text for option 1");
+                $("#serverText").html("Server originally run by Rory.  Runs on Minecraft 1.7.10.  Requires the following:\n<a href=\"https://files.minecraftforge.net/maven/net/minecraftforge/forge/index_1.7.10.html\">Forge Modloader 1.7.10</a>\n<a href=\"https://drive.google.com/open?id=1LsWumfkbwimx8a58t6UnC64iyqjzQKZj\">Mods</a>");
                 break;
             case "2":
                 $("#serverText").html("html/text for option 2");
