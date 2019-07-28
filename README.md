468ab18a3075f6a83ff5ec6ea95978927ce630f9
Liam <lbonnage@gmail.com>
2019-07-28 01:16:07 -0400

    Started creating server

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
 # On-Demand Minecraft Server for Kennett Group
 
-Python Flask/AWS application, creates a website that can be used to launch an AWS EC2 instance to host a Minecraft server.  Users can request for the server to start through the webpage.  The server will shut down automatically after 15 minutes when there are no players., in order to reduce hosting costs.
+Python Flask/AWS application, creates a website that can be used to launch an AWS EC2 instance to host a Minecraft server.  Users can request for the server to start through the webpage.  The server will shut down automatically after 15 minutes when there are no players, in order to reduce hosting costs.
 
 
-# AWS Setup
+## AWS Setup
 
 
-# Web Application Deployment
+## Web Application Deployment
 
 
-# AWS Instance Configuration
+## AWS Instance Configuration
+
+
+## Goals
+ - Choice of server to run
+ - Current player count
+ - If the server is already running it should tell you when you enter the password
+ - Better security (maybe?)
--- server.py
+++ server.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS, cross_origin
+from multiprocessing import Process
 from configuration import Config
+import json
+import boto3
+import time
+import paramiko
+import os
 
 app = Flask(__name__)
 CORS(app)
 
+#Paraminko SSH information
+dirname = os.path.dirname(__file__)
+filename = os.path.join(dirname, Config.SSH_KEY_FILE_PATH)
+key = paramiko.RSAKey.from_private_key_file(filename)
+sshClient = paramiko.SSHClient()
+sshClient.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 
 ##
@@ -13,3 +25,35 @@ CORS(app)
 @app.route("/")
 def loadIndex():
 	return render_template('index.html')
+
+
+##
+# Receives the request to initialize the server.
+# If the password is correct and the server isn't running, starts the server.
+# If the password is correct and the server is running, returns the IP address.
+# If the password is incorrect, returns a fail message to the browser.
+##
+@app.route('/initserver', methods=['POST'])
+def init_server():
+
+	inputted_password = request.form['password']
+
+	return_data = {}
+
+	message = 'Password Incorrect'
+
+	if inputted_password == Config.SERVER_PASSWORD:
+
+		#Instantiate the EC2 Server here
+        client = boto3.client(
+            'ec2',
+            aws_access_key_id=Config.ACCESS_KEY,
+            aws_secret_access_key=Config.SECRET_KEY,
+            region_name=Config.EC2_REGION
+        )
+
+        # ipaddress = manageServer(client)
+
+    ipaddress = 'fake'
+    print("Successfully started EC2 server with IP: " _+ ipaddress)
+    return render_template('index.html', ipMessage=ipaddress)
