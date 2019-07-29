53481e6a23ce8af63f70e4eb7c71a0cfdc213552
Liam <lbonnage@gmail.com>
2019-07-28 21:16:42 -0400

    Working on descriptions

--- static/scripts/dropdownScript.js
+++ static/scripts/dropdownScript.js
@@ -6,7 +6,9 @@ $(function () {
             		function() {
             			var forgelink = "<a href=\"https://files.minecraftforge.net/maven/net/minecraftforge/forge/index_1.7.10.html\">Forge Modloader 1.7.10</a>";
             			var modslink = "<a href=\"https://drive.google.com/open?id=1LsWumfkbwimx8a58t6UnC64iyqjzQKZj\">Mods</a>";
-            			return "Server originally run by Rory.  Runs on Minecraft 1.7.10.  Requires the following: ";
+            			return "Server originally run by Rory.  Runs on Minecraft 1.7.10.  Requires the following: \n" +
+            			forgelink + "\n" +
+            			modslink;
             		}
             		)
                 break;
