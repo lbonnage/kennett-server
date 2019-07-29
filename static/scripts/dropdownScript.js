a93c906dfeb98123b45ed5cd9ca2c28690be66d1
Liam <lbonnage@gmail.com>
2019-07-28 21:13:40 -0400

    Attempting to make good server descriptions

--- static/scripts/dropdownScript.js
+++ static/scripts/dropdownScript.js
@@ -2,7 +2,13 @@ $(function () {
     $("#serverDropdown").on("change", function () {
         switch ($(this).val()) {
             case "1":
-                $("#serverText").html("Server originally run by Rory.  Runs on Minecraft 1.7.10.  Requires the following:\n<a href=\"https://files.minecraftforge.net/maven/net/minecraftforge/forge/index_1.7.10.html\">Forge Modloader 1.7.10</a>\n<a href=\"https://drive.google.com/open?id=1LsWumfkbwimx8a58t6UnC64iyqjzQKZj\">Mods</a>");
+            	$("#serverText").html(
+            		function() {
+            			var forgelink = "<a href=\"https://files.minecraftforge.net/maven/net/minecraftforge/forge/index_1.7.10.html\">Forge Modloader 1.7.10</a>";
+            			var modslink = "<a href=\"https://drive.google.com/open?id=1LsWumfkbwimx8a58t6UnC64iyqjzQKZj\">Mods</a>";
+            			return "Server originally run by Rory.  Runs on Minecraft 1.7.10.  Requires the following: ";
+            		}
+            		)
                 break;
             case "2":
                 $("#serverText").html("html/text for option 2");
