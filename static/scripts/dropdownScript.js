7c2190bcdcab8ce6f21503d6d03f5cbe6949a513
Liam <lbonnage@gmail.com>
2019-07-28 21:24:23 -0400

    Finished adding servers

--- static/scripts/dropdownScript.js
+++ static/scripts/dropdownScript.js
@@ -1,25 +1,28 @@
 $(function () {
     $("#serverDropdown").on("change", function () {
         switch ($(this).val()) {
+        	case "0":
+                $("#serverText").html("Select an option to see server details");
+                break;
             case "1":
             	$("#serverText").html(
             		function() {
             			var forgelink = "<a href=\"https://files.minecraftforge.net/maven/net/minecraftforge/forge/index_1.7.10.html\">Forge Modloader 1.7.10</a>";
             			var modslink = "<a href=\"https://drive.google.com/open?id=1LsWumfkbwimx8a58t6UnC64iyqjzQKZj\">Mods</a>";
-            			return "Server originally run by Rory.  Runs on Minecraft 1.7.10.  Requires the following: \n" +
-            			forgelink + "\n" +
+            			return "Server originally run by Rory.  Runs on Minecraft 1.7.10.  Requires the following: " +
+            			forgelink + ", " +
             			modslink;
             		}
             		)
                 break;
             case "2":
-                $("#serverText").html("html/text for option 2");
-                break;
-            case "3":
-                $("#serverText").html("html/text for option 3");
-                break;
-            case "4":
-                $("#serverText").html("html/text for option 4");
+            	$("#serverText").html(
+            		function() {
+            			var forgelink = "<a href=\"https://www.curseforge.com/minecraft/modpacks/roguelike-adventures-and-dungeons\">Modpack</a>";
+            			return "Roguelike Adventures and Dungeons modpack.  Runs on Minecraft 1.12.2.  If installing through Twitch launcher (easiest method) disregard following. Requires the following: " +
+            			forgelink;
+            		}
+            		)
                 break;
         }
     });
