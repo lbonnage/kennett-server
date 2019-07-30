412f192478c4107fd4c7cfdfb65713c85df2fa59
Liam <lbonnage@gmail.com>
2019-07-29 21:46:38 -0400

    Fixed server path

--- server.py
+++ server.py
@@ -34,7 +34,12 @@ def init_server_commands(instance_ip, server):
 		print('[Server] About to execute command with server jar_path: ' + jar_path)
 
 		# Execute a Unix command via SSH after connecting to the instance
-		stdin, stdout, stderr = ssh_client.exec_command("cd " + server_path + "; screen -dmS minecraft bash -c 'sudo java " + Config.MEMORY_ALLOCATION + "-jar " + server_path + "nogui' ; cd ../..")
+
+		cmd = "cd " + jar_path + "; screen -dmS minecraft bash -c 'sudo java " + Config.MEMORY_ALLOCATION + "-jar " + server_path + "nogui' ; cd ../.."
+		print('[Server] Executing cmd through SSH: ')
+		print(cmd)
+
+		stdin, stdout, stderr = ssh_client.exec_command(cmd)
 		print("[Server] Command executed")
 		print('[Server] stdin: ' + stdin)
 		print('[Server] stdout: ' + stdout)
