1b9c6c7eab079f1bd7d0e627677ca81bb5ee32df
Liam <lbonnage@gmail.com>
2019-08-01 21:13:07 -0400

    Added twitch link

--- static/scripts/dropdownScript.js
+++ static/scripts/dropdownScript.js
@@ -27,9 +27,11 @@ $(function () {
 			case "3":
             	$("#serverText").html(
             		function() {
-            			var forgelink = "<a href=\"https://www.curseforge.com/minecraft/modpacks/sevtech-ages/files/2744522\">Modpack</a>";
+            			var forgelink = "<a href=\"https://www.curseforge.com/minecraft/modpacks/sevtech-ages/files/2744522\">Modpack</a>"
+						var twitchlink = "<a href=\"https://www.curseforge.com/minecraft/modpacks/sevtech-ages/download/2744522?client=y\">Twitch launcher download</a>"
             			return "SevTech Ages 3.1.2 modpack.  Runs on Minecraft 1.12.2.  If installing through Twitch launcher (easiest method) disregard following. Requires the following: " +
-            			forgelink;
+            			forgelink + " / " +
+						twitchlink;
             		}
             		)
                 break;
