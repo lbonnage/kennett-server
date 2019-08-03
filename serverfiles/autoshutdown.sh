acc4b9d7159f00b4b692a4aba12ff8398a135ae5
Liam <lbonnage@gmail.com>
2019-08-03 13:10:56 -0400

    Updated instances

--- serverfiles/autoshutdown.sh
+++ serverfiles/autoshutdown.sh
@@ -3,8 +3,8 @@ now=$(date)
 echo "$now Starting autoshutdown"
 SERVICE='server.jar'
 if ps ax | grep -v grep | grep $SERVICE > /dev/null; then
-    echo "In the for loop"
-    PLAYERSEMPTY=" There are 0"
+	echo "In the for loop"
+	PLAYERSEMPTY=" There are 0"
 	$(screen -S minecraft -p 0 -X stuff "list^M")
 	sleep 5
 	$(screen -S minecraft -p 0 -X stuff "list^M")
@@ -12,10 +12,9 @@ if ps ax | grep -v grep | grep $SERVICE > /dev/null; then
 	echo "About to get player list"
 	SERVERPATH=$(sudo tail -n 1 /home/ubuntu/serverpath.txt)
 	echo "Using serverpath ${SERVERPATH}"
-
-	PLAYERSLIST=$(sudo perl -ne '$l=$_ if /There are/; END{print $l}' /home/ubuntu/${SERVERPATH}/logs/latest.log | cut -f2 -d"/" | cut -f2 -d ":")
-    echo "Retrieved player list:"
-    echo $PLAYERSLIST
+	PLAYERSLIST=$(sudo perl -ne '$l=$_ if /There are/; END{print $l}' /home/ubuntu${SERVERPATH}/logs/latest.log | cut -f2 -d"/" | cut -f2 -d ":")
+	echo "Retrieved player list:"
+	echo $PLAYERSLIST
 	if [ "$PLAYERSLIST" = "$PLAYERSEMPTY" ]
 	then
 		echo "Waiting for players to come back in 12m, otherwise shutdown"
@@ -24,11 +23,19 @@ if ps ax | grep -v grep | grep $SERVICE > /dev/null; then
 		sleep 5
 		$(screen -S minecraft -p 0 -X stuff "list^M")
 		sleep 5
-		PLAYERSLIST=$(sudo perl -ne '$l=$_ if /There are/; END{print $l}' /home/ubuntu/${SERVERPATH}/logs/latest.log | cut -f2 -d"/" | cut -f2 -d ":")
-        echo "Retrieved final chance player list:"
-        echo $PLAYERSLIST		
-        if [ "$PLAYERSLIST" = "$PLAYERSEMPTY" ]
+		PLAYERSLIST=$(sudo perl -ne '$l=$_ if /There are/; END{print $l}' /home/ubuntu${SERVERPATH}/logs/latest.log | cut -f2 -d"/" | cut -f2 -d ":")
+		echo "Retrieved final chance player list:"
+		echo $PLAYERSLIST
+		if [ "$PLAYERSLIST" = "$PLAYERSEMPTY" ]
 		then
+			echo "Server is empty and shutdown will be initiated, first shutting down Minecraft server gracefully"
+			$(screen -S minecraft -p 0 -X stuff "stop^M")
+			while ps ax | grep -v grep | grep $SERVICE > /dev/null
+			do
+				echo "Server still shutting down, waiting for it to shut down gracefully"
+				sleep 1m
+			done
+			echo "Shutting down instance"
 			$(sudo /sbin/shutdown -P +1)
 		fi
 	fi
@@ -36,7 +43,7 @@ else
 	echo "Screen does not exist, briefly waiting before trying again"
 	sleep 10m
 	if ! ps ax | grep -v grep | grep $SERVICE > /dev/null; then
-		echo "Screen does not exist, shutting down"
+		echo "Screen does not exist, shutting down server"
 		$(sudo /sbin/shutdown -P +1)
 	fi
 fi
