24b5fb49d3841f8f1e1842613ee76f8381a2b626
Liam <lbonnage@gmail.com>
2019-08-04 11:21:57 -0400

    Adding ragnamod

--- README.md
+++ README.md
@@ -10,6 +10,25 @@ Python Flask/AWS application, creates a website that can be used to launch an AW
 
 
 ## AWS Instance Configuration
+1. Connect through SSH to the server using preferred client (Putty, MobaXTerm, etc.)
+2. Make the ubuntu user admin if it isn't already with
+`adduser ubuntu sudo`
+3. Install Java JDK onto the instance.  Use
+`sudo apt install openjdk-8-jdk-headless`.
+You may need to run
+`sudo apt-get update`
+to update package list.
+4. Using a FTP client (such as FileZilla) connect to the same address with the same username you used to connect to the SSH client.  Drag all the files from the `serverfiles` directory into the `/home/ubuntu` directory of the instance.
+5. Download your desired Minecraft servers and rename the executable that you would normally run to start the server to `server.jar`.
+6. Input the following to the SSH client **exactly**:
+`sudo mkdir screens`
+`mkdir servers`
+Following this, create a directory in the `servers` directory for each type of server you are trying to be able to run using `mkdir servers/{servername}`.  Then, copy the server files into the corresponding directory using the FTP client.  Continue with the follow inputs.
+`sudo chmod 777 servers`
+`sudo chmod 700 /home/ubuntu`
+`export SCREENDIR=/home/ubuntu/screens`
+`crontab /home/ubuntu/crontab`
+7. Following this, you can just restart.
 
 
 ## Goals
--- static/scripts/dropdownScript.js
+++ static/scripts/dropdownScript.js
@@ -9,9 +9,8 @@ $(function () {
             		function() {
             			var forgelink = "<a href=\"https://files.minecraftforge.net/maven/net/minecraftforge/forge/index_1.7.10.html\">Forge Modloader 1.7.10</a>";
             			var modslink = "<a href=\"https://drive.google.com/open?id=1LsWumfkbwimx8a58t6UnC64iyqjzQKZj\">Mods</a>";
-            			return "Server originally run by Rory.  Runs on Minecraft 1.7.10.<br>Requires the following: " +
-            			forgelink + ", " +
-            			modslink;
+            			return "Server originally run by Rory.  Runs on Minecraft 1.7.10.<br>" +
+							"Requires the following: " + forgelink + ", " + modslink;
             		}
             		)
                 break;
@@ -19,8 +18,11 @@ $(function () {
             	$("#serverText").html(
             		function() {
             			var forgelink = "<a href=\"https://www.curseforge.com/minecraft/modpacks/roguelike-adventures-and-dungeons\">Modpack</a>";
-            			return "Roguelike Adventures and Dungeons modpack.  Runs on Minecraft 1.12.2.<br>If installing through Twitch launcher (easiest method) disregard following.<br>Requires the following: " +
-            			forgelink;
+            			return "Roguelike Adventures and Dungeons modpack.  Runs on Minecraft 1.12.2.<br>" +
+							"Modpack focused on adventuring, exploration, looting, fighting.  Not so techy.<br>" +
+							"Has randomly generated dungeons, 8 dimensions, RPG-style levelling and magic system, harder mobs, quests, airships, more weapons and items.<br>" +
+							"If installing through Twitch launcher (easiest method) disregard following.<br>" +
+							"Requires the following: " + forgelink;
             		}
             		)
                 break;
@@ -28,8 +30,21 @@ $(function () {
             	$("#serverText").html(
             		function() {
             			var forgelink = "<a href=\"https://www.curseforge.com/minecraft/modpacks/sevtech-ages/files/2744522\">Modpack</a>"
-            			return "SevTech Ages 3.1.2 modpack.  Runs on Minecraft 1.12.2.<br>If installing through Twitch launcher (easiest method), make sure to click 'Versions' and download '3.1.2', as the default may be '3.0.8' for some, and disregard the following.<br>Requires the following: " +
-            			forgelink;
+            			return "SevTech Ages 3.1.2 modpack.  Runs on Minecraft 1.12.2.<br>" +
+							"Basically a progression modpack featuring a ton of mods that you have to unlock by 'progressing' through the ages, working your way from primitive to eventually futuristic.<br>" +
+							"If installing through Twitch launcher (easiest method), make sure to click 'Versions' and download '3.1.2', as the default may be wrong for some, and disregard the following.<br>" +
+							"Requires the following: " + forgelink;
+            		}
+            		)
+                break;
+			case "4":
+            	$("#serverText").html(
+            		function() {
+            			var forgelink = "<a href=\"https://www.curseforge.com/minecraft/modpacks/ragnamod-v/files/2752080\">Modpack</a>"
+            			return "Ragnamod V.  Runs on Minecraft 1.12.2.<br>" +
+							"Tbh idk what this is, it's mainly used by French people, but its in English and the mod list looks amazing and its super popular.<br>" +
+							"If installing through Twitch launcher (easiest method), disregard the following.<br>" +
+							"Requires the following: " + forgelink;
             		}
             		)
                 break;
